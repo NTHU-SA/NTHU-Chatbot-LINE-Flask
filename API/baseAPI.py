bf36cb054d612daba8293ebcf511c58473b44664
lawrence910426 <lawrence910426@gmail.com>
2022-03-21 10:44:00 -0700

    Inject env vars

--- API/baseAPI.py
+++ API/baseAPI.py
@@ -2,7 +2,7 @@ import requests
 
 class API:
     def __init__(self):
-        self.URL = 'http://34.120.176.132/api/v1'
+        self.URL = 'http://${GO_IP}/api/v1'
         #self.URL = 'http://127.0.0.1:5000/api/v1'
         self.liff_URL = 'https://liff-nthu-chatbot.ml/api/v1'
         
