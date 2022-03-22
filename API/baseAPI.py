d60c70df3024a753d49ac2bc94fbd35d2c0a09f0
lawrence910426 <lawrence910426@gmail.com>
2022-03-22 05:02:48 -0700

    Different domain name

--- API/baseAPI.py
+++ API/baseAPI.py
@@ -2,7 +2,7 @@ import requests
 
 class API:
     def __init__(self):
-        self.URL = 'http://${GINIP}/api/v1'
+        self.URL = 'http://linebot.nthusa.tw/api/v1'
         #self.URL = 'http://127.0.0.1:5000/api/v1'
         self.liff_URL = 'https://liff-nthu-chatbot.ml/api/v1'
         
