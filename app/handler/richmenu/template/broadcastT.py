b2fb861bf3639ddbb324d6ebd99b35d44a25647a
lawrence910426 <lawrence910426@gmail.com>
2022-05-12 16:44:37 +0800

    feat: Change to finished materials

--- app/handler/richmenu/template/broadcastT.py
+++ app/handler/richmenu/template/broadcastT.py
@@ -6,7 +6,7 @@ def broadcast_info(toggle): #公車路線清單
             alt_text='公車路線清單',
             template=ButtonsTemplate(
                 title='是否開啟主動推播？',
-                thumbnail_image_url='https://raw.githubusercontent.com/NTHU-SA/NTHU-Campus-Agent-LINE-Flask/master/images/dogOn_cropped.png',
+                thumbnail_image_url='https://github.com/NTHU-SA/NTHU-Campus-Agent-LINE-Flask/blob/master/images/Richmenu-images/on-cropped.png?raw=true',
                 text='目前主動推播已開啟',
                 actions=[
                     MessageAction(label = "關閉主動推播", text = "[選單]關閉主動推播")
@@ -18,7 +18,7 @@ def broadcast_info(toggle): #公車路線清單
             alt_text='公車路線清單',
             template=ButtonsTemplate(
                 title='是否開啟主動推播？',
-                thumbnail_image_url='https://raw.githubusercontent.com/NTHU-SA/NTHU-Campus-Agent-LINE-Flask/master/images/dogOff_cropped.PNG',
+                thumbnail_image_url='https://raw.githubusercontent.com/NTHU-SA/NTHU-Campus-Agent-LINE-Flask/master/images/Richmenu-images/off-cropped.png',
                 text='目前主動推播已關閉',
                 actions=[
                     MessageAction(label = "開啟主動推播", text = "[選單]開啟主動推播")
