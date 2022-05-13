356064436982be1da69890d6c447c66ae6b73bd7
lawrence910426 <lawrence910426@gmail.com>
2022-05-13 12:04:28 +0800

    fix: Show entire image

--- app/handler/richmenu/template/broadcastT.py
+++ app/handler/richmenu/template/broadcastT.py
@@ -7,6 +7,7 @@ def broadcast_info(toggle):
             template=ButtonsTemplate(
                 title='是否開啟主動推播？',
                 thumbnail_image_url='https://github.com/NTHU-SA/NTHU-Campus-Agent-LINE-Flask/blob/master/images/Richmenu-images/on-cropped.png?raw=true',
+                imageSize='contain',
                 text='目前主動推播已開啟',
                 actions=[
                     MessageAction(label = "確認關閉主動推播", text = "[選單]關閉主動推播")
@@ -19,6 +20,7 @@ def broadcast_info(toggle):
             template=ButtonsTemplate(
                 title='是否開啟主動推播？',
                 thumbnail_image_url='https://raw.githubusercontent.com/NTHU-SA/NTHU-Campus-Agent-LINE-Flask/master/images/Richmenu-images/off-cropped.png',
+                imageSize='contain',
                 text='目前主動推播已關閉',
                 actions=[
                     MessageAction(label = "確認開啟主動推播", text = "[選單]開啟主動推播")
